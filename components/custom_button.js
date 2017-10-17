e5b5d63becedd6e70478185d370c08036e676b57
Logan James Kaser <lkaser@e1z1r3p42.42.us.org>
2017-10-17 12:24:10 -0700

    FIX: Code style, MOD: Switch Button to CustomButton

---
+++ components/custom_button.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet, View } from 'react-native';
+
+export default class CustomButton extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = props;
+	}
+	render() {
+		const { onPress, title } = this.props;
+		return (
+		<TouchableOpacity onPress={onPress} style={styles.button}>
+			<View style={styles.textBox}>
+				<Text style={styles.text}>{title}</Text>
+			</View>
+		</TouchableOpacity>
+	);
+	}
+}
+
+const styles = StyleSheet.create({
+	button: {
+		flex: 0.13,
+		backgroundColor: "#2AA198"
+	},
+	textBox: {
+		flex: 1,
+		justifyContent: 'center'
+	},
+	text: {
+		fontSize: 22,
+		color: "#FFFFFF",
+		textAlign: 'center'
+	}
+});
