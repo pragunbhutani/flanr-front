20e9f179acf107e542e084b5d602ff0bf867fea4
Logan James Kaser <lkaser@e1z1r3p42.42.us.org>
2017-10-17 11:40:34 -0700

    MOD: Style Changes.

--- App.js
+++ App.js
@@ -1,37 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-import { DrawerNavigator, StackNavigator } from 'react-navigation';
+import { StyleSheet, View } from 'react-native';
+import { DrawerNavigator } from 'react-navigation';
 import MapScreen from './screens/map.js';
 import MyPlacesScreen from './screens/myPlaces.js';
 import SearchScreen from './screens/search.js';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 24,
-    backgroundColor: '#000',
-  },
+	container: {
+		flex: 1,
+		paddingTop: 24,
+		backgroundColor: '#000'
+	},
 });
 
 const MyApp = DrawerNavigator({
-  	Home: {
-    	screen: MapScreen,
-  	},
-  	MyPlaces: {
-    	screen: MyPlacesScreen,
-  	},
-  	Search: {
-  		screen: SearchScreen,
-  	}
+	Home: {
+		screen: MapScreen
+	},
+	MyPlaces: {
+		screen: MyPlacesScreen
+	},
+	Search: {
+		screen: SearchScreen
+	}
 });
 
 export default class App extends React.Component {
 	render() {
 		return (
 			<View style={styles.container}>
-    			<MyApp />
-  		  	</View>
-    	);
+				<MyApp />
+			</View>
+		);
 	}
 }
